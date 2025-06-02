56742528712d654675c3eefec10cc1f643562c67
aleksei_front-dev <aleksei.frontdev@gmail.com>
2025-06-02 16:34:45 +0300

    change time roulette

--- src/constants/rouletteGame.ts
+++ src/constants/rouletteGame.ts
@@ -1,4 +1,4 @@
 export const SPIN_DURATION = 3000;
 export const MIN_LOOPS = 3;
-export const AUTO_SPIN_INTERVAL = 10 * 1000; // 10 секунд для тестирования
+export const AUTO_SPIN_INTERVAL = 60 * 1000; // 10 секунд для тестирования
 export const ACTIVE_CLASS_DURATION = 2000; // 2 секунды
